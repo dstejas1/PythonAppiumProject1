c07e50b5918855a087373309f1b5de79f9b3419c
tejasd <dstejas1@gmail.com>
2026-07-21 20:40:37 +0530

    Scroll Upto and Scroll horizontal and vertical SrolltillEnd

---
+++ basics/UIScrollableUIselector.py
@@ -0,0 +1,53 @@
+import time
+from concurrent.futures import thread
+
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.actions import pointer_input, interaction
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.actions.pointer_input import PointerInput
+from selenium.webdriver.common.by import By
+
+desired_caps = {
+    "platformName": "Android",
+    "platformVersion": "15",
+    "deviceName": "TejasEmulator",
+    "automationName": "UiAutomator2",
+    "app": "/Users/tejasd/Downloads/ApiDemos-debug.apk",
+    "appActivity": "io.appium.android.apis.ApiDemos"
+}
+
+options = UiAutomator2Options().load_capabilities(desired_caps)
+
+driver = webdriver.Remote(
+    "http://127.0.0.1:4723",
+    options=options
+)
+
+driver.find_element(AppiumBy.ACCESSIBILITY_ID,'Views').click()
+time.sleep(2)
+
+ui_scrollable = (
+    'new UiScrollable(new UiSelector().scrollable(true))'
+    '.scrollIntoView(new UiSelector().text("Lists"))'
+)
+
+driver.find_element(
+    AppiumBy.ANDROID_UIAUTOMATOR,
+    ui_scrollable
+)
+driver.find_element(AppiumBy.ACCESSIBILITY_ID,'Lists').click()
+time.sleep(2)
+driver.find_element(AppiumBy.ACCESSIBILITY_ID,'01. Array').click()
+
+ui_scrollable1 = (
+    'new UiScrollable(new UiSelector().scrollable(true))'
+    '.setAsVerticalList()'
+    '.scrollIntoView(new UiSelector().text("Curd"))'
+)
+
+driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR,ui_scrollable1)
+time.sleep(5)
+driver.quit()
